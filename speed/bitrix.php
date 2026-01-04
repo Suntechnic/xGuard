1db95c8539fa694e32aa8bf8215440a8c2d66a04
store123-dev <dev@test-store123.ru>
2026-01-04 12:01:58 +0300

    nope и bitrix

---
+++ speed/bitrix.php
@@ -0,0 +1,13 @@
+<?php
+include __DIR__ . '/.prolog.php';
+
+$UID = 'bitrix'; // тест установки соединения с базой данных
+
+
+// подключим ядро битрикс
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+$I++;
+
+
+
+include __DIR__ . '/.epilog.php';
